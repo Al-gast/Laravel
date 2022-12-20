4c6bca67702771f0116c7435d52bb1494529da53
Al-gast <agastyo2004@gmail.com>
2022-12-20 14:06:56 +0700

    add controller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
@@ -29,16 +30,6 @@ Route::get('/about', function () {
     ]);
 });
 
-Route::get('/blog', function () {
-    return view('posts', [
-        "title" => "post",
-        "posts" => Post::all()
-    ]);
-});
+Route::get('/blog', [PostController::class, 'index']);
 
-Route::get('post/{slug}', function ($slug) {
-    return view('post', [
-        "title" => "post",
-        "post" => Post::find($slug)
-    ]);
-});
+Route::get('post/{slug}', [PostController::class, 'show']);
