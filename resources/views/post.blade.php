862258c5b53ed4e39d22983de52727efdba2a02d
Al-gast <agastyo2004@gmail.com>
2022-12-21 11:23:20 +0700

    add table category and relatioship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,6 +3,7 @@
 @section('container')
 <article>
     <h2>{{ $post->title }}</h2>
+    <p>By. {{ $post->author }} in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
     {!! $post->body !!}
 </article>
 <a href="/blog">Back To Posts</a>
@@ -10,9 +11,10 @@
 
 
 {{-- Post::create([
-    'title' => 'Second Title',
+    'title' => 'Third Title',
+    'category_id' => 2,
     'author' => 'Atha',
-    'slug' => 'second-title',
+    'slug' => 'third-title',
     'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos id dicta ipsam iure illum eum rem itaque praesentium mollitia. Natus deserunt adipisci quibusdam! Nulla delectus ab animi quae! Fuga,',
     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eos id dicta ipsam iure illum eum rem itaque praesentium mollitia. Natus deserunt adipisci quibusdam! Nulla delectus ab animi quae! Fuga, iste? Vero minus, accusamus alias, voluptates eligendi, ab nesciunt omnis explicabo illo repellendus suscipit dicta nisi temporibus consequatur earum.</p><p>Adipisci nihil tempore expedita voluptatibus veniam, illum molestiae ab iusto cupiditate officiis asperiores maiores iste repellendus harum optio vero at illo a ullam temporibus quia excepturi facere? Dolores, magni deleniti dolore reiciendis possimus, sequi recusandae excepturi maiores qui ea optio dicta iusto non! Nam sequi earum id rerum repellendus labore quidem quod assumenda numquam culpa laborum natus saepe,</p><p>officia a doloremque cupiditate libero magnam sapiente! Fugit, autem cupiditate magni modi culpa, quia vel obcaecati aut repudiandae incidunt aliquam qui veniam, unde in? Incidunt fugit cumque asperiores, ut atque ad quia dignissimos placeat, in labore quo? Ut, architecto facilis dolore aperiam earum commodi?</p>'
 ]) --}}
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home',[
+    return view('home', [
         "title" => "home"
     ]);
 });
@@ -31,5 +32,19 @@ Route::get('/about', function () {
 });
 
 Route::get('/blog', [PostController::class, 'index']);
-
 Route::get('post/{post:slug}', [PostController::class, 'show']);
+
+Route::get('/categories', function() {
+    return view('categories', [
+        "title" => "Post Categories",
+        "categories" => Category::all()
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function(Category $category) {
+    return view('category', [
+        "title" => $category->name,
+        "posts" => $category->posts,
+        "category" => $category->name
+    ]);
+});
